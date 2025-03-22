3f09cf9e2c5ece58aab2a45b4b944a819d79edce
kevin.falencik <kevin.falencik@staffcircle.com>
2025-03-22 15:31:23 +0000

    services

--- components/Services.vue
+++ components/Services.vue
@@ -2,7 +2,7 @@
     <section id="services" class="bg-lightGrey" style="overflow: hidden">
         <div class="max-w-8xl mx-auto text-center">
             <h2 class="mb-0">Services</h2>
-            <small>With years of expertise in the industry, we have successfully delivered projects of all scales and complexities. Find out what our company can do for you</small>
+            <small>With years of expertise in the industry, we have successfully delivered projects of all scales and complexities. At every step of the way, from design to completion, we are with you.</small>
             <div class="services-wrapper my-4 mt-10" :class="[{'columns-2': viewport.matches('tablet', 'desktop')}, {'columns-3': viewport.isGreaterOrEquals('desktopMedium')}]">
                 <div v-for="(service, item) in services" :key="`service-${index}`" class="aspect-square py-10 text-center px-6">
                     <i class="text-5xl text-secondary" :class="service.icon"></i>
@@ -20,34 +20,34 @@
     const services = computed(() => {
         return [
         {
-            icon: 'bi-people',
-            heading: 'General Contracting',
-            subheading: 'We have a long list of professional contractors, with whom our engineers and architects enjoy to work on a majority of our projects!'
-        },
-        {
-            icon: 'bi-building',
-            heading: 'Building Information Modeling',
-            subheading: 'Oftentimes physical and functional essence of any construction project needs to be represented digitally, in a 3D model format.'
-        }
-        ,{
             icon: 'bi-palette',
-            heading: 'Design & Build',
-            subheading: 'If a project is not too complex, we may hire a designer-builder type of contractor, to make the longevity of the construction shorter.'
+            heading: 'Interior Design',
+            subheading: 'We offer comprehensive interior design services, from initial concepts to final solutions.'
+        },
+        {
+            icon: 'bi-columns-gap',
+            heading: 'Floor Finishing',
+            subheading: 'We handle the installation of various types of floors, including wooden, stone, and laminate, ensuring durability and aesthetics.'
         }
         ,{
-            icon: ' bi-tools',
-            heading: 'Construction Services',
-            subheading: 'Our customers love the pace/quality tempo that we show during each of the principal construction processes!'
-        },
-        {
-            icon: 'bi-hammer',
-            heading: 'Pre-construction Services',
-            subheading: 'We take our time on initial planning before any construction begins, to balance all the financial and efficiency issues beforehand…'
-        },
-        {
             icon: 'bi-bricks',
-            heading: 'Construction Management',
-            subheading: 'Professional construction project management is essential for facilitating and directing the entire process to achieve ideal results.'
+            heading: 'Walls and Ceilings',
+            subheading: 'We provide wall partition installation, painting, wallpapering, and ceiling finishing, including suspended ceilings.'
+        }
+        ,{
+            icon: ' bi-door-closed',
+            heading: 'Windows and Doors',
+            subheading: 'We ensure professional installation and replacement of windows and doors, focusing on acoustic and thermal insulation.'
+        },
+        {
+            icon: 'bi-droplet-half',
+            heading: 'Kitchens and Bathrooms',
+            subheading: 'We carry out complete kitchen and bathroom arrangements, from design to installation, tailored to your individual preferences.'
+        },
+        {
+            icon: 'bi-lightning-charge',
+            heading: 'Electrical and Plumbing Work',
+            subheading: 'We perform electrical and plumbing installations, modernizations, and repairs.'
         }
     ]
     })
