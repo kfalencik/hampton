fcc0ee48bb64af9ec9cc24000abd4d83d047f298
kevin.falencik <kevin.falencik@staffcircle.com>
2025-03-22 12:53:22 +0000

    better intro

--- components/Hello.vue
+++ components/Hello.vue
@@ -2,9 +2,9 @@
     <div id="about-us" class="max-w-8xl p-5 py-15 mx-auto">
         <div class="max-w-4xl">
             <div class="divider-block">
-                <h2>Hampton is one of the nation`s foremost construction companies</h2>
+                <h2>Hampton Construction Group is a renowned construction company based in Naples, Florida</h2>
                 <div>
-                    <p class="text-black">Hampton Construction Group is a renowned construction company based in Naples, Florida, founded by three friends who combined their unique experiences to create a company capable of handling the most demanding projects. </p>
+                    <p class="text-black">Founded by three friends who combined their unique experiences to create a company capable of handling the most demanding projects. </p>
                     <p>We specialize in home construction and high-end renovations, with a strong emphasis on top-quality and precision. Our projects are distinguished by attention to detail, the use of the finest materials, and unique solutions that meet the expectations of even the most discerning clients.</p>
                 </div>
             </div>
