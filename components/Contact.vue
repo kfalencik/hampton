27b1e388940f9627f47a9d91eaeb44fb7c1cb10e
kevin.falencik <kevin.falencik@staffcircle.com>
2025-03-26 10:57:20 +0000

    name field

--- components/Contact.vue
+++ components/Contact.vue
@@ -6,6 +6,7 @@
             <p class="text-white mx-auto mt-2 mb-16">Great projects start with great people. Our team is built on trust, skill, and a shared commitment to doing things the right way—bringing vision and expertise to every job.</p>
         </div>
         <v-form method="post" name="contact" v-model="valid" netlify>
+            <input type="hidden" name="form-name" value="contact" />
             <input type="hidden" name="subject" data-remove-prefix value="Inquiry from Hampton Construction group" />
             <v-text-field class="mb-3" type="text" name="name" id="name" placeholder="Name" variant="solo-filled" required :rules="[rules.rules.required]" />
             <v-text-field class="mb-3" type="email" name="email" id="email" placeholder="Your Email Address" hint="This is so we can reply to your message" variant="solo-filled" required :rules="[rules.rules.required, rules.rules.email]" />
